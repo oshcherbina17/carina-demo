b58b50cd10d97e3b70888dcd8bec553448b517ea
Olha Shcherbina <olhashcherbina@MacBook-Pro-Olha.local>
2023-02-01 13:16:44 +0200

    replace import

--- src/main/java/com/qaprosoft/carina/demo/web/gui/desktop/PCTablesPage.java
+++ src/main/java/com/qaprosoft/carina/demo/web/gui/desktop/PCTablesPage.java
@@ -2,13 +2,13 @@ package com.qaprosoft.carina.demo.web.gui.desktop;
 
 import java.util.List;
 
-import com.qaprosoft.carina.demo.web.enums.FilterType;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.WebDriver;
 
 import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.demo.web.enums.FilterType;
 import com.qaprosoft.carina.demo.web.gui.common.PCTablesPageBase;
 import com.qaprosoft.carina.demo.web.gui.common.TableItemsPageBase;
 
